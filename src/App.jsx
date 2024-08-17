0c981d1cd2a340b4f0c407d04ed7fca7da913e5c
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-08-17 09:47:44 +0200

    Best Practice: Storing Components in Files & Using a Good Project Structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,6 @@
-import reactImg from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from './data';
-
-const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max + 1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-
-  return (
-    <header>
-      <img src={reactImg} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>
-        {description} React concepts you will need for almost any app you are going to build!
-      </p>
-    </header>
-  );
-}
-
-// function CoreConcept(props) {
-function CoreConcept({title, description, image}) {
-  return (
-    <li>
-      <img src={image} alt={title} />
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  )
-    {/* <li>
-      <img src={props.image} alt={props.title} />
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li> */}
-}
+import Header from './components/Header';
+import CoreConcept from './components/CoreConcepts';
 
 function App() {
   return (
---
+++ src/components/CoreConcepts.jsx
@@ -0,0 +1,15 @@
+// function CoreConcept(props) {
+export default function CoreConcept({ title, description, image }) {
+    return (
+        <li>
+            <img src={image} alt={title} />
+            <h3>{title}</h3>
+            <p>{description}</p>
+        </li>
+    )
+    {/* <li>
+            <img src={props.image} alt={props.title} />
+            <h3>{props.title}</h3>
+            <p>{props.description}</p>
+          </li> */}
+}
