e4c0ac352bab366abaf401fc144b52f54504ae07
GlazMichelFranckDodo225 <cda.gmfd@gmail.com>
2024-09-02 10:09:20 +0200

    Reacting to Events

--- src/components/TabButton/TabButton.jsx
+++ src/components/TabButton/TabButton.jsx
@@ -1,10 +1,20 @@
 // export default function TabButton(props) {
-export default function TabButton({children}) {
+export default function TabButton({ children }) {
+    // In Regular JS, to Add a Event Listener
+    /* document.querySelector('button').addEventListener('click', 
+        () => {
+
+        }
+    ) */
+
+    function handleClick() {
+        console.log("Hi !");
+    }
+
     return (
-        <>
-            {/* <li><button>{props.children}</button></li> */}
-            <li><button>{children}</button></li>
-        </>
+        <li>
+            <button onClick={handleClick}>{children}</button>
+        </li>
     )
 }
 
